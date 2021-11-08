12e8a8c147de6e2bf862d25281a4199e3cb405ce
Muhammed <mxgardeee@gmail.com>
2021-11-08 22:12:46 +0200

    Adding a Button and a Counter to the React Counter Component

---
+++ Java Full Stack with Spring Boot and React/Section03, Step03/Components/Counter/Counter.jsx
@@ -0,0 +1,38 @@
+import React, {Component} from 'react'
+import './Counter.css'
+
+class Counter extends Component {  
+  
+  //Define the initial state in a constructor
+  //state => counter 0
+  constructor() {
+      super(); //Error 1
+
+      this.state = {
+          counter : 0
+      }
+
+      this.increment = this.increment.bind(this);
+  }
+  
+  render() {
+    return (
+        <div className="counter">
+        <button onClick={this.increment} >+1</button>
+        <span className="count">{this.state.counter}</span>
+        </div>
+    )
+  }
+  
+  increment() { //Update state - counter++
+    //console.log('increment');
+    //this.state.counter++; //Bad Practice
+    this.setState({
+        counter: this.state.counter + 1
+    });
+  }
+
+}
+
+
+export default Counter
