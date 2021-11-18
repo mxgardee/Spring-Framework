c4172ec59a328cadfac9ccdef71fc71e5346d5bd
Muhammed <mxgardeee@gmail.com>
2021-11-18 10:03:25 +0200

    Creating React Axios Interceptor to add Basic Auth Header

---
+++ Java Full Stack with Spring Boot and React/Section07, Step62/frontend/todo-app/src/components/todo/AuthenticationService.js
@@ -0,0 +1,44 @@
+import axios from 'axios'
+
+class AuthenticationService {
+
+    registerSuccessfulLogin(username,password){
+        console.log('registerSuccessfulLogin')
+        sessionStorage.setItem('authenticatedUser', username)
+        this.setupAxiosInterceptors()
+    }
+
+    logout() {
+        sessionStorage.removeItem('authenticatedUser');
+    }
+
+    isUserLoggedIn() {
+        let user = sessionStorage.getItem('authenticatedUser')
+        if(user===null) return false
+        return true
+    }
+
+    getLoggedInUserName() {
+        let user = sessionStorage.getItem('authenticatedUser')
+        if(user===null) return ''
+        return user
+    }
+
+    setupAxiosInterceptors() {
+        let username = 'in28minutes'
+        let password = 'dummy'
+
+        let basicAuthHeader = 'Basic ' +  window.btoa(username + ":" + password)
+
+        axios.interceptors.request.use(
+            (config) => {
+                if(this.isUserLoggedIn()) {
+                    config.headers.authorization = basicAuthHeader
+                }
+                return config
+            }
+        )
+    }
+}
+
+export default new AuthenticationService()
